45aae15a9d8f04ed3b6369216638445c9bf398b7
Osman Pérez <osman2907@gmail.com>
2022-03-28 18:49:43 -0400

    Integración con bootstrap y Jquery

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 
 app.listen(8080, () => console.log("Servidor express activo http://localhost:8080"));
 
+app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap/dist/`));
+app.use("/jquery", express.static(`${__dirname}/node_modules/jquery/dist/`));
+
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`);
 })
