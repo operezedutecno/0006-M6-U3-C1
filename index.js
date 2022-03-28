21ff2d36202152cc3828e5c866ad834fe04b133f
Osman Pérez <osman2907@gmail.com>
2022-03-28 19:01:43 -0400

    Avances configuración axios

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 app.listen(8080, () => console.log("Servidor express activo http://localhost:8080"));
 
 app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap/dist/`));
 app.use("/jquery", express.static(`${__dirname}/node_modules/jquery/dist/`));
 
+const urlAPI = "https://randomuser.me/api/";
+
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`);
+})
+
+app.get("/registrar", (req, res) => {
+
 })
